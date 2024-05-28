97fbeb584f2a000db01a72ab28fd11f8aded844b
AlibeyovaFarida <129386607+AlibeyovaFarida@users.noreply.github.com>
2024-05-29 00:12:10 +0400

    Initial Commit

---
+++ HomeTask-13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeTask-13
+//
+//  Created by Apple on 29.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
